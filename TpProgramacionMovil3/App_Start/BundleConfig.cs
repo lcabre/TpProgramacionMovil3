90f855e8db880ce71b5550ac884046cf551aed26
lcabre <leocab16@gmail.com>
2018-09-11 20:39:03 -0300

    Initial commit

---
+++ TpProgramacionMovil3/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TpProgramacionMovil3
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/jquery/jquery-3.3.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalidate").Include(
+                        "~/js/jquery/jquery.validate.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/bootstrap/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/css/bootstrap/bootstrap.min.css",
+                      "~/css/fontawesome/css/all.min.css",
+                      "~/css/estilos.css"));
+        }
+    }
+}
---
+++ TpProgramacionMovil3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TpProgramacionMovil3.MvcApplication" Language="C#" %>
